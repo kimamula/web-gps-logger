dec2e33f536f08133d59306997248fa8f216c6a0
kimamula <kenji@localhost.localdomain>
2014-11-01 19:41:14 +0900

    gitignore built files

--- public/components/wgl/gps-log/gps-log.js
+++
@@ -1,2 +0,0 @@
-var Wgl;!function(){"use strict"}(Wgl||(Wgl={}));
-//# sourceMappingURL=../gps-log/gps-log.js.map
--- test/components/wgl/ref.ts
+++
@@ -1,3 +0,0 @@
-///<reference path='../../../public/components/wgl/gps-log/gps-log.ts'/>
-///<reference path='../../../public/components/wgl/gps-logger/gps-logger.ts'/>
-///<reference path='../../../public/components/wgl/gps-log-localstorage-repository/gps-log-localstorage-repository.ts'/>
